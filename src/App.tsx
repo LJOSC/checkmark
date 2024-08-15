f0f1d178a8538b3578039725f0e8d7b48c3f86e7
Harsh Dobariya <harshdobariya79@gmail.com>
2024-08-15 12:01:20 +0530

    Resolve merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import './App.css';
 import 'src/assets/styles/main.scss';
 
 function App() {
-  return <div>Checkmark</div>;
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <div className="text-6xl text-gray-600 font-semibold">Checkmark</div>
+    </div>
+  );
 }
 
 export default App;
